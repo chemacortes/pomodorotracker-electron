d5bd87f40e04472f3b6b579c997825c454e40005
chemacortes <dev.xtrem@gmail.com>
2017-03-16 11:51:46 +0100

    First version by vmarting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# pomodorotracker-electron
+# Pomodoro Tracker - electron
+
 Packing the [PomodoroTracker webapp](https://pomodoro-tracker.com/) into an electron application
+
+For build the standalone application, introduce within a shell:
+
+    $ yarn
+	$ ./node_packages/.bin/electron-packager ./ --all
+
+Or select your specific architecture.
---
+++ pomodoroTracker.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const {app, BrowserWindow, globalShortcut} = electron;
+
+let mainWindow;
+
+app.on('ready',() =>{
+	mainWindow = new BrowserWindow({
+		width:1000,
+		height:700
+	});
+	mainWindow.setTitle('Pomodoro Tracker');
+	mainWindow.loadURL('https://pomodoro-tracker.com/')
+	
+	mainWindow.on('closed',() =>{
+			mainWindow = null;
+	});
+	
+});
